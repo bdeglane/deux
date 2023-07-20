eca6edc7a08ea7220150fee33ce1815c32234716
Benoit Deglane <benoit_deglane@connect-tech.sncf>
2023-07-20 15:51:26 +0200

    :wrench: Add versions file

--- modules/domains/reservation/packages/reservation/build.gradle.kts
+++ modules/domains/reservation/packages/reservation/build.gradle.kts
@@ -12,28 +12,9 @@ dependencies {
 
     testImplementation("deux.reservation.model:lib")
 
-    // testImplementation(platform("org.junit:junit-bom:5.9.3"))
-    // testImplementation("org.junit.jupiter:junit-jupiter")
-    // val version = "5.6.1"
-    // testImplementation("io.kotest:kotest-runner-junit5:$version")
-    // testImplementation("io.kotest:kotest-assertions-core:$version")
-    // testImplementation("io.kotest:kotest-property:$version")
-
-    // val version = "5.6.1"
-    // testImplementation("io.kotest:kotest-runner-junit5:$version")
-    // testImplementation("io.kotest:kotest-assertions-core:$version")
-    // testImplementation("io.kotest:kotest-property:$version")
-
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation(libs.kotlinTestJunit5)
+    testImplementation(libs.junitJupiterEngine)
 }
-
-// tasks.withType<Test>().configureEach {
-//     useJUnitPlatform()
-// }
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
--- modules/domains/reservation/packages/settings.gradle.kts
+++ modules/domains/reservation/packages/settings.gradle.kts
@@ -7,3 +7,13 @@ include(
 includeBuild("../model")
 includeBuild("../../../utils")
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../../../../versions/libs.versions.toml"))
+        }
+    }
+}
